701a639bb3413b889c584ea67812875a2d108eee
Ruteski <lruteski@gmail.com>
2022-05-31 20:39:05 -0300

    movimentacao

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private Rigidbody2D _rb;
+    private Vector2 _direction;
+
+    private void Start() {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() {
+        _direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    private void FixedUpdate() {
+        //_rb.position = _direction * _speed;
+        _rb.MovePosition(_rb.position + _direction * _speed * Time.deltaTime);
+    }
+}
