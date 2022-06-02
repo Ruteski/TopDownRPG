0c4be59788f835c4c8b3c86bf565766d3a082ac0
Ruteski <lruteski@gmail.com>
2022-06-02 17:29:44 -0300

    IA NPC parte 2 - criado movimentacao pelo path

--- Assets/Scripts/NPC/NPC.cs
+++ Assets/Scripts/NPC/NPC.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private List<Transform> _paths = new List<Transform>();
+    [SerializeField] private float _speed;
 
-    // Update is called once per frame
+    private int _index;
+
     void Update()
     {
-        
+        transform.position = Vector2.MoveTowards(transform.position, _paths[_index].position, _speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, _paths[_index].position) < 0.1f) {
+            if (_index < _paths.Count -1) { 
+                _index++;
+            } else {
+                _index = 0;
+            }
+        }
+
+        Vector2 direction = _paths[_index].position - transform.position;
+
+        if (direction.x > 0) {
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+
+        if (direction.x < 0) {
+            transform.eulerAngles = new Vector2(0, 180);
+        }
     }
 }
