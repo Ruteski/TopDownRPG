2b34b589c261d068cf37eec7e66a6aca1aa96055
Ruteski <lruteski@gmail.com>
2022-06-07 22:16:41 -0300

    regar e colher

--- Assets/Scripts/Farm/SlotController.cs
+++ Assets/Scripts/Farm/SlotController.cs
@@ -4,16 +4,46 @@ using UnityEngine;
 
 public class SlotController : MonoBehaviour
 {
+    [Header("Components")]
     [SerializeField] private SpriteRenderer _spriteRenderer;
     [SerializeField] private Sprite _hole;
     [SerializeField] private Sprite _carrot;
 
+    [Header("Settings")]
     [SerializeField] private int _digAmount;// qtd de vezes de escavacao para criar o buraco
+    [Tooltip("Total de agua para para nascer uma cenoura")]
+    [SerializeField] private int _waterAmount;// total de agua para para nascer uma cenoura
+    [SerializeField] private bool _detecting;// se estou regando a o canteiro
+
+    private Items _playerItems;
 
     private int _initialDigAmount;
+    private float _currentWater;
+    private bool _dugHole;//buraco cavado
 
     private void Start() {
         _initialDigAmount = _digAmount;
+        _playerItems = FindObjectOfType<Items>();
+    }
+
+    private void Update() {
+        if (_dugHole) {
+            if (_detecting) {
+                _currentWater += 0.01f;
+            }
+
+            //nasce uma cenoura
+            if (_currentWater >= _waterAmount) {
+                _spriteRenderer.sprite = _carrot;
+
+                //coletar a cenoura
+                if (Input.GetKeyDown(KeyCode.E)) {
+                    _spriteRenderer.sprite = _hole;
+                    _playerItems.Carrots++;
+                    _currentWater = 0f;
+                }
+            }
+        }
     }
 
     public void OnHit() {
@@ -21,16 +51,23 @@ public class SlotController : MonoBehaviour
 
         if (_digAmount <= _initialDigAmount / 2) {
             _spriteRenderer.sprite = _hole;
+            _dugHole = true;
         }
-
-        //if (_digAmount <= 0) {
-        //    _spriteRenderer.sprite = _carrot;
-        //}
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.CompareTag("Dig")) {
             OnHit();
+        }
+
+        if (collision.CompareTag("Watering")) {
+            _detecting = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.CompareTag("Watering")) {
+            _detecting = false;
         }
     }
 
--- Assets/Scripts/Player/Items.cs
+++ Assets/Scripts/Player/Items.cs
@@ -6,11 +6,13 @@ public class Items : MonoBehaviour
 {
     [SerializeField] private int _totalWood;
     [SerializeField] private float _currentWater;
+    [SerializeField] private int _carrots;
 
     private float _waterLimit = 50;
 
     public int TotalWood { get => _totalWood; set => _totalWood = value; }
     public float TotalWater { get => _currentWater; set => _currentWater = value; }
+    public int Carrots { get => _carrots; set => _carrots = value; }
 
     public void WaterLimit(float water) { 
         if (_currentWater < _waterLimit) {
