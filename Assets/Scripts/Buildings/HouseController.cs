9dc851f717170b0d9c5e2068fb0e9b1decadf264
Ruteski <lruteski@gmail.com>
2022-06-15 20:37:27 -0300

    construcao - utilizando insumos

--- Assets/Scripts/Buildings/HouseController.cs
+++ Assets/Scripts/Buildings/HouseController.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class HouseController : MonoBehaviour
 {
+    [Header("Amounts")]
+    [SerializeField] private int _woodAmount;
+    [SerializeField] private float _timeAmount; // qtd de tempo para finalizar a casa
+    [SerializeField] private Color _startColor;
+    [SerializeField] private Color _endColor;
+
+    [Header("Components")]
     [SerializeField] private GameObject _houseCollider;
     [SerializeField] private SpriteRenderer _houseSprite;
     [SerializeField] private Transform _point;
-    [SerializeField] private Color _startColor;
-    [SerializeField] private Color _endColor;
-    [SerializeField] private float _timeAmount; // qtd de tempo para finalizar a casa
 
     private PlayerController _player;
     private PlayerAnim _playerAnim;
+    private Items _playerItems;
 
     private bool _detectingPlayer;
     private float _timeCount;
@@ -23,15 +28,18 @@ public class HouseController : MonoBehaviour
         // só funciona pq so tenho 1 playerItems na cena toda
         _player = FindObjectOfType<PlayerController>();
         _playerAnim = _player.GetComponent<PlayerAnim>();
+        _playerItems = _player.GetComponent<Items>();
     }
 
     private void Update() {
-        if (_detectingPlayer && Input.GetKeyDown(KeyCode.E)) {
+        if (_detectingPlayer && Input.GetKeyDown(KeyCode.E) && (_playerItems.TotalWood >= _woodAmount)) {
+            //inicializa a construcao
             _isBegining = true;
             _playerAnim.OnHammeringStarted();
             _houseSprite.color = _startColor;
             _player.transform.position = _point.transform.position;
             _player.isPaused = true;
+            _playerItems.TotalWood -= _woodAmount;
         }
 
         if (_isBegining) {
