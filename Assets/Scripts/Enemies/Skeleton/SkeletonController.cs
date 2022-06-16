b207d1655477c03bdeff436b56534f14de96b2c1
Ruteski <lruteski@gmail.com>
2022-06-16 14:12:07 -0300

    combate - adicionado comentario para o link de download do navmesh

--- Assets/Scripts/Enemies/Skeleton/SkeletonController.cs
+++ Assets/Scripts/Enemies/Skeleton/SkeletonController.cs
@@ -2,6 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/*
+ * LINK PARA DOWNLOAD DO NAVMESHPLUS 2D
+ *  https://github.com/h8man/NavMeshPlus 
+ */
+
 public class SkeletonController : MonoBehaviour
 {
 
